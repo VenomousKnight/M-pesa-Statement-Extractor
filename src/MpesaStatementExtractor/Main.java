d7d0031cd5a461574fbd81ab60a7380e1a72ba1b
Emmanuel Simon <musavili2022@gmail.com>
2024-10-28 15:24:24 +0300

    Closed unsused resources after execution

--- src/MpesaStatementExtractor/Main.java
+++ src/MpesaStatementExtractor/Main.java
@@ -54,7 +54,8 @@ public class Main {
 							if (rows.indexOf(row) == 0)
 								continue;
 
-							StringBuilder sql = new StringBuilder("INSERT INTO transactions (receipt_no,completion_time,");
+							StringBuilder sql = new StringBuilder(
+									"INSERT INTO transactions (receipt_no,completion_time,");
 							StringBuilder values = new StringBuilder("VALUES (?,?,");
 
 							List<Object> parameters = new ArrayList<>();
@@ -62,103 +63,56 @@ public class Main {
 
 							// Get text from each cell
 							for (int j = 0; j < row.size() - 1; j++) {
-								switch(j){
-								case(0):
-										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
-										break;
-								case(1):
-										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
-										break;
-								case(2):
-									if(row.get(j).getText().trim() != "") {
+								switch (j) {
+								case (0):
+									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+									break;
+								case (1):
+									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+									break;
+								case (2):
+									if (row.get(j).getText().trim() != "") {
 										sql.append("details,");
 										values.append("?,");
-										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replaceAll("[\\s\\p{Z}\\p{C}]+", "").replaceAll("\\t", " ").replaceAll("\\s+", " "));
+										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "")
+												.replaceAll("[\\s\\p{Z}\\p{C}]+", "").replaceAll("\\t", " ")
+												.replaceAll("\\s+", " "));
 									}
-									
+
 									break;
-									
-								case(3):
-										sql.append("transaction_status,");
-										values.append("?,");
-										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
-										
-										break;
-								case(4):
+
+								case (3):
+									sql.append("transaction_status,");
+									values.append("?,");
+									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+
+									break;
+								case (4):
 									if (row.get(j).getText().trim() != "") {
 										sql.append("paid_in,");
 										values.append("?,");
-										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
+										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "")
+												.replace(",", ""));
 
 									}
 									break;
-								case(5):
+								case (5):
 									if (row.get(j).getText().trim() != "") {
 										sql.append("withdrawn,");
 										values.append("?,");
-										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
+										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "")
+												.replace(",", ""));
 
 									}
 									break;
-								case(6):
+								case (6):
 									sql.append("balance) ");
 									values.append("?)");
-									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
-									
+									parameters.add(
+											row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
+
 									break;
 								}
-//								if (j == 0) {
-//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
-//								}
-//								if (j == 1) {
-//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
-//
-//								}
-//								if (row.get(j).getText().trim() != "" && j == 2) {
-//									sql.append("details,");
-//									values.append("?,");
-//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replaceAll("[\\s\\p{Z}\\p{C}]+", "").replaceAll("\\t", " ").replaceAll("\\s+", " "));
-//								}
-//								if (j == 3) {
-//									sql.append("transaction_status,");
-//									values.append("?,");
-//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
-//								}
-//
-//								if (row.get(j).getText().trim() != "" && j == 4) {
-//									sql.append("paid_in,");
-//									values.append("?,");
-//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
-//
-//								}
-//								if (row.get(j).getText().trim() != "" && j == 5) {
-//									sql.append("withdrawn,");
-//									values.append("?,");
-//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
-//
-//								}
-//								if (j == 6) {
-//									sql.append("balance) ");
-//									values.append("?)");
-//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
-//
-//								}
-								
-								
-								
-								
-								
-								
-//								if(row.get(j).getText().trim() == "")
-//									continue;
-//								if(j > 3) {
-//									Double cellAmount = Double.valueOf(row.get(j).getText().trim());
-//									pstmt.setDouble(j + 1, cellAmount);
-//								}
-//								else {
-//									String cellText = row.get(j).getText().trim();
-//									pstmt.setString(j + 1, cellText);
-//								}
 
 							}
 							sql.append(values.toString());
@@ -167,29 +121,23 @@ public class Main {
 							PreparedStatement pstmt = conn.prepareStatement(sql.toString());
 							for (int l = 0; l < parameters.size(); l++) {
 								pstmt.setObject(l + 1, parameters.get(l));
-								System.out.println(l+ 1);
+								System.out.println(l + 1);
 
 							}
-							//pstmt.addBatch();
-//							
 							pstmt.execute();
-//							System.out.println();
-//							
-						}
+							pstmt.close();
 
-//						
+						}
 
 					}
 
 				}
-
-//				pstmt.close();
-//				conn.close();
+				conn.close();
 
 			}
 
-//			oe.close();
-//			document.close();
+			oe.close();
+			document.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
