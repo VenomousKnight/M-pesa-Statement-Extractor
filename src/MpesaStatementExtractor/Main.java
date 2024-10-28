bb344d52d63beb32607e58ce1027fb2a080fd5d2
Emmanuel Simon <musavili2022@gmail.com>
2024-10-28 15:18:46 +0300

    Replaced if statement for logic with switch

--- src/MpesaStatementExtractor/Main.java
+++ src/MpesaStatementExtractor/Main.java
@@ -62,42 +62,93 @@ public class Main {
 
 							// Get text from each cell
 							for (int j = 0; j < row.size() - 1; j++) {
-								if (j == 0) {
-									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
-								}
-								if (j == 1) {
-									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+								switch(j){
+								case(0):
+										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+										break;
+								case(1):
+										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+										break;
+								case(2):
+									if(row.get(j).getText().trim() != "") {
+										sql.append("details,");
+										values.append("?,");
+										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replaceAll("[\\s\\p{Z}\\p{C}]+", "").replaceAll("\\t", " ").replaceAll("\\s+", " "));
+									}
+									
+									break;
+									
+								case(3):
+										sql.append("transaction_status,");
+										values.append("?,");
+										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+										
+										break;
+								case(4):
+									if (row.get(j).getText().trim() != "") {
+										sql.append("paid_in,");
+										values.append("?,");
+										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
 
-								}
-								if (row.get(j).getText().trim() != "" && j == 2) {
-									sql.append("details,");
-									values.append("?,");
-									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replaceAll("[\\s\\p{Z}\\p{C}]+", "").replaceAll("\\t", " ").replaceAll("\\s+", " "));
-								}
-								if (j == 3) {
-									sql.append("transaction_status,");
-									values.append("?,");
-									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
-								}
+									}
+									break;
+								case(5):
+									if (row.get(j).getText().trim() != "") {
+										sql.append("withdrawn,");
+										values.append("?,");
+										parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
 
-								if (row.get(j).getText().trim() != "" && j == 4) {
-									sql.append("paid_in,");
-									values.append("?,");
-									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
-
-								}
-								if (row.get(j).getText().trim() != "" && j == 5) {
-									sql.append("withdrawn,");
-									values.append("?,");
-									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
-
-								}
-								if (j == 6) {
+									}
+									break;
+								case(6):
 									sql.append("balance) ");
 									values.append("?)");
 									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
-
+									
+									break;
 								}
+//								if (j == 0) {
+//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+//								}
+//								if (j == 1) {
+//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+//
+//								}
+//								if (row.get(j).getText().trim() != "" && j == 2) {
+//									sql.append("details,");
+//									values.append("?,");
+//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replaceAll("[\\s\\p{Z}\\p{C}]+", "").replaceAll("\\t", " ").replaceAll("\\s+", " "));
+//								}
+//								if (j == 3) {
+//									sql.append("transaction_status,");
+//									values.append("?,");
+//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", ""));
+//								}
+//
+//								if (row.get(j).getText().trim() != "" && j == 4) {
+//									sql.append("paid_in,");
+//									values.append("?,");
+//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
+//
+//								}
+//								if (row.get(j).getText().trim() != "" && j == 5) {
+//									sql.append("withdrawn,");
+//									values.append("?,");
+//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
+//
+//								}
+//								if (j == 6) {
+//									sql.append("balance) ");
+//									values.append("?)");
+//									parameters.add(row.get(j).getText().trim().replaceAll("\\r?\\n|\\r", "").replace(",", ""));
+//
+//								}
+								
+								
+								
+								
+								
+								
 //								if(row.get(j).getText().trim() == "")
 //									continue;
 //								if(j > 3) {
